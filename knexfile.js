6f90705deea28490225636d9a0a5c24136a16883
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 16:43:43 -0500

    created exports object

--- knexfile.js
+++ knexfile.js
@@ -16,3 +16,22 @@ const sharedConfig = {
     seeds: { directory: '.api/data/seeds' }
 }
 
+
+module.exports = {
+    development: {
+        ...sharedConfig,
+        connection: process.env.DEV_DATABASE_URL
+
+    },
+
+    testing: {
+        ...sharedConfig,
+        connection: process.env.TESTING_DATABASE_URL
+    },
+    production: {
+        ...sharedConfig,
+        connection: process.env.DATABASE_URL,
+        pool: { min: 2, max: 10 }
+    }
+};
+
