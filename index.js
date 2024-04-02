bc515cce204a706a9181d25de91ead37fc8f6c1f
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 16:54:02 -0500

    implemented server.listen on port

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000 // this is fallback for testing
 
 const server = require('./api/server.js')
+
+
+server.listen(PORT, () => {
+    console.log('Listening on port ${PORT}')
+})
