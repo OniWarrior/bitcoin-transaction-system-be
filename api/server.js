3dca3f54c7556eb8e8ffb4f6276fc52588bd3511
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 16:59:40 -0500

    created local vars for auth user routers and express server

--- api/server.js
+++ api/server.js
@@ -2,3 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const helmet = require('helmet')
+
+const { restricted } = require('../api/auth/auth-middleware')
+
+const authRouter = require('../api/auth/auth-router')
+const userRouter = require('../api/auth/user-router')
+
+const server = express()
