b508a56c3a37249d3f72f52fe18d675a2a6a3ead
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-05 14:43:47 -0500

    created local var to collect user data

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -13,6 +13,10 @@ const User = require('../users/user-model')
 // path to register new account
 router.post('/Signup', checkIfEmailAlreadyRegistered, checkForMissingEmailOrPassword, async (req, res, next) => {
     try {
+        let user = req.body
+
+
+
 
     }
     catch (err) {
