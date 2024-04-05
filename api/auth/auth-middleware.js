57238693a386fc593e228527ff1ecb57db4b7c7c
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-04 20:19:42 -0500

    401 if no token exists

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,5 +6,8 @@ const User = require('../users/user-model.js')
 const restricted = (req, res, next) => {
     const token = req.headers.authorization
 
+    if (!token) {
+        res.status(401).json("Token required")
+    }
 
 }
