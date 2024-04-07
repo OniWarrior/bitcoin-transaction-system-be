0978d81e3951cbfec910081ae6b7560c2b1c9906
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-07 18:37:32 -0500

    created successful response for retrieval

--- api/users/client-router.js
+++ api/users/client-router.js
@@ -4,6 +4,10 @@ const { default: jwtDecode } = require('jwt-decode')
 const { restricted } = require('../auth/auth-middleware')
 
 // retrieve all past orders for client
-router.get('/Orders', restricted, async (req, res, next) => {
+router.get('/Orders', restricted, (req, res, next) => {
+    Client.retrievePastOrders()
+        .then((response) => {
+            res.status(200).json(response)
+        })
 
 })
