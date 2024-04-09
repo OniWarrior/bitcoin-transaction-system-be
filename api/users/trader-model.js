affd6fa3394558084f690544c1548b1ca2b7334a
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-09 15:06:50 -0500

    created if search block

--- api/users/trader-model.js
+++ api/users/trader-model.js
@@ -30,24 +30,7 @@ function updateTransferAccountById(traderId, USD) {
         .update('USD', USD)
 }
 
-// For trader--find the client based on entering the first name and last name 
-// of client
-async function findClientByFullName(client) {
-    const foundClient = await db('Client')
-        .select([
-            'client_id',
-            'first_name',
-            'last_name',
-            'email'])
-        .where({
-            first_name: client.first_name,
-            last_name: client.last_name
-        })
-        .first()
-    return foundClient
 
-
-}
 
 
 // For trader--find the client based on entering their email
@@ -108,7 +91,6 @@ function addTransacOrPayment(cancelled) {
 module.exports = {
     findClientByEmail,
     findClientByEmailAndFullName,
-    findClientByFullName,
     addTransacOrPayment,
     updateTransferAccountById,
     updateUSDBalanceOfTrader,
--- api/users/trader-router.js
+++ api/users/trader-router.js
@@ -8,6 +8,19 @@ const Trader = require('./trader-model')
 // path to retrieve client in search
 router.get('/FindClient/:client_id', restricted, async (req, res, next) => {
     try {
+        // get client search credentials
+        const client = req.body
+
+        if (!client.first_name && !client.last_name && client.email) {
+
+        }
+        else if (client.first_name && client.last_name && client.email) {
+
+        }
+        else if (client.email) {
+
+        }
+
 
     }
     catch (err) {
