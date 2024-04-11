a6926858b26e2cae5deefcfde9c55890e460e5b1
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-11 12:37:22 -0500

    completed retrieval of weekly transactions query

--- api/users/manager-model.js
+++ api/users/manager-model.js
@@ -2,17 +2,24 @@ const db = require('../data/dbConfig')
 
 
 
-// Manager op -- retrieve all orders made on the day provided
+// Manager op -- retrieve total number orders made on the day provided
+// note-- dates are inputted by manager
 async function retrieveDailyTransactions(todaysDate) {
     const transactions = await db('Order')
-        .select([
-            'client_id',
-            'date',
-            'comm_paid',
-            'comm_type',
-            'Bitcoin_balance',
-            'isCancelled'
-        ])
+        .count('order_id')
         .where('date', todaysDate)
+        .first()
     return transactions
+}
+
+// Manager op--retrieve all orders made on the current week
+// note-- dates are inputted by manager
+async function retrieveWeeklyTransactions(startDate, endDate) {
+    const transactions = await db('Order')
+        .count('order_id')
+        .whereBetween('date', [startDate, endDate])
+        .first()
+    return transactions
+
+
 }
