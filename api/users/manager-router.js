6fb94aa198f38e3f798b3f30f75db2b50656c8d4
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-11 14:48:58 -0500

    completed crud ops for transactions retrieval for month

--- api/users/manager-router.js
+++ api/users/manager-router.js
@@ -40,3 +40,22 @@ router.get('/total-weekly-transactions', restricted, async (req, res, next) => {
             .json(`Server Error: ${err.message}`)
     }
 })
+
+// path to retrieve total number of transactions for the month
+router.get('/total-monthly-transactions', restricted, async (req, res, next) => {
+    try {
+        const date = req.body
+        const totalMonthlyTransactions = await Manager.retrieveMonthlyTransactions(date.month, date.year)
+
+        if (totalMonthlyTransactions) {
+            res.status(200)
+                .json(`Server Error: ${err.message}`)
+        }
+
+    } catch (err) {
+        res.status(500)
+            .json(`Server Error: ${err.message}`)
+    }
+})
+
+module.exports = router
