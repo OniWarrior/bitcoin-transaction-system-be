b75fdf985c0b285e7deccc73380ad5803255424d
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-10 22:04:32 -0500

    completed daily transactions query

--- api/users/manager-model.js
+++ api/users/manager-model.js
@@ -1,1 +1,18 @@
 const db = require('../data/dbConfig')
+
+
+
+// Manager op -- retrieve all orders made on the day provided
+async function retrieveDailyTransactions(todaysDate) {
+    const transactions = await db('Order')
+        .select([
+            'client_id',
+            'date',
+            'comm_paid',
+            'comm_type',
+            'Bitcoin_balance',
+            'isCancelled'
+        ])
+        .where('date', todaysDate)
+    return transactions
+}
--- api/users/manager-router.js
+++ api/users/manager-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
-
+const Manager = require('./manager-model')
 const { default: jwtDecode } = require('jwt-decode')
 const { restricted } = require('../auth/auth-middleware')
+
+
+// path to retrieve total transactions
