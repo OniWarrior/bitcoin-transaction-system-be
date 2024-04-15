83563470b2a00c1a2b861bb10e1e3d4517b6575a
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-14 20:10:32 -0500

    start fresh before tests

---
+++ api/__tests__/server.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest')
+const server = require('../server')
+const db = require('../data/dbConfig')
+
+// start with a fresh unaltered db before doing main tests
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
