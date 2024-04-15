75d5e1b6a3ee852a13336c93b4fe365d0fc558a3
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-14 19:47:53 -0500

    completed dummy data for dummy cancel log

---
+++ api/data/seeds/06-cancelled.js
@@ -0,0 +1,402 @@
+
+const cancelled = [
+    {
+        log_id: 1,
+        order_id: 2,
+        client_id: 1,
+        trader_id: 1,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 32614.50,
+        comm_type: 'USD',
+        Bitcoin_balance: 5.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+
+    },
+    {
+        log_id: 2,
+        order_id: 4,
+        client_id: 2,
+        trader_id: 1,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.60,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 6,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 3,
+        order_id: 6,
+        client_id: 3,
+        trader_id: 1,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.4,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 8.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 4,
+        order_id: 8,
+        client_id: 4,
+        trader_id: 1,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 19568.70,
+        comm_type: 'USD',
+        Bitcoin_balance: 3.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 5,
+        order_id: 10,
+        client_id: 5,
+        trader_id: 1,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.50,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 5.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 6,
+        order_id: 12,
+        client_id: 6,
+        trader_id: 2,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.30,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 6.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 7,
+        order_id: 14,
+        client_id: 7,
+        trader_id: 2,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.8,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 8.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 8,
+        order_id: 16,
+        client_id: 8,
+        trader_id: 2,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.1,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 2.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 9,
+        order_id: 18,
+        client_id: 9,
+        trader_id: 2,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.5,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 5.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 10,
+        order_id: 20,
+        client_id: 10,
+        trader_id: 2,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.6,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 6.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 11,
+        order_id: 22,
+        client_id: 11,
+        trader_id: 3,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 65229.00,
+        comm_type: 'USD',
+        Bitcoin_balance: 10.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 12,
+        order_id: 24,
+        client_id: 12,
+        trader_id: 3,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 16307.25,
+        comm_type: 'USD',
+        Bitcoin_balance: 5.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 13,
+        order_id: 26,
+        client_id: 13,
+        trader_id: 3,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.3,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 3.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 14,
+        order_id: 28,
+        client_id: 14,
+        trader_id: 3,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.15,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 3.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 15,
+        order_id: 30,
+        client_id: 15,
+        trader_id: 3,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 65229.00,
+        comm_type: 'USD',
+        Bitcoin_balance: 10.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 16,
+        order_id: 32,
+        client_id: 16,
+        trader_id: 4,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.50,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 5.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 17,
+        order_id: 34,
+        client_id: 17,
+        trader_id: 4,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.2,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 2.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 18,
+        order_id: 36,
+        client_id: 18,
+        trader_id: 4,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 0.05,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 1.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 19,
+        order_id: 38,
+        client_id: 19,
+        trader_id: 4,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 1.00,
+        comm_type: 'Bitcoin',
+        Bitcoin_balance: 10.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 20,
+        order_id: 40,
+        client_id: 20,
+        trader_id: 4,
+        transac_id: null,
+        date: new Date().getDate(),
+        comm_paid: 32614.50,
+        comm_type: 'USD',
+        Bitcoin_balance: 5.00,
+        amount_paid: 0.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 21,
+        order_id: null,
+        client_id: 3,
+        trader_id: 1,
+        transac_id: 3,
+        date: new Date().getDate(),
+        comm_paid: 0.00,
+        comm_type: null,
+        Bitcoin_balance: 0.00,
+        amount_paid: 60000.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 22,
+        order_id: null,
+        client_id: 6,
+        trader_id: 2,
+        transac_id: 6,
+        date: new Date().getDate(),
+        comm_paid: 0.00,
+        comm_type: null,
+        Bitcoin_balance: 0.00,
+        amount_paid: 15000.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 23,
+        order_id: null,
+        client_id: 7,
+        trader_id: 2,
+        transac_id: 7,
+        date: new Date().getDate(),
+        comm_paid: 0.00,
+        comm_type: null,
+        Bitcoin_balance: 0.00,
+        amount_paid: 10000.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 24,
+        order_id: null,
+        client_id: 11,
+        trader_id: 3,
+        transac_id: 11,
+        date: new Date().getDate(),
+        comm_paid: 0.00,
+        comm_type: null,
+        Bitcoin_balance: 0.00,
+        amount_paid: 5000.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 25,
+        order_id: null,
+        client_id: 13,
+        trader_id: 3,
+        transac_id: 13,
+        date: new Date().getDate(),
+        comm_paid: 0.00,
+        comm_type: null,
+        Bitcoin_balance: 0.00,
+        amount_paid: 10000.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+    {
+        log_id: 26,
+        order_id: null,
+        client_id: 18,
+        trader_id: 4,
+        transac_id: 18,
+        date: new Date().getDate(),
+        comm_paid: 0.00,
+        comm_type: null,
+        Bitcoin_balance: 0.00,
+        amount_paid: 30000.00,
+        isCancelled: true,
+        isInvested: false
+
+    },
+
+
+
+
+]
+
+exports.seed = function (knex) {
+    return knex('Cancel_log').insert(cancelled)
+}
