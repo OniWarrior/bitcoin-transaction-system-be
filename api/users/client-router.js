d3cb7069543a5417b583d7f987db4431174ae89f
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 15:08:28 -0500

    d

--- api/users/client-router.js
+++ api/users/client-router.js
@@ -466,7 +466,7 @@ router.post('/TransferMoney', async (req, res, next) => {
 
         // update the usd balance of the client
         const reducedBalance = client.USD_balance - transfer.amount_paid
-        res.status(200).json({ message: 'here', reduced: reducedBalance })
+
         const updateUSDBalance = await Client.updateUSDBalance(decoded.email, reducedBalance)
 
         // update the transfer account of the trader
