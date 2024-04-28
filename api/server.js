97d2b8b27db212f0476033eee5bba9b177f1b220
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-27 19:01:44 -0500

    fd

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const authRouter = require('../api/auth/auth-router')
 const clientRouter = require('../api/users/client-router')
 const traderRouter = require('../api/users/trader-router')
 const managerRouter = require('../api/users/manager-router')
-
+const cryptoRouter = require('../api/users/crypto-router')
 
 const server = express()
 
@@ -23,6 +23,6 @@ server.use(cookieParser())
 
 server.use('/api/auth', authRouter)
 
-server.use('/api/users', clientRouter, traderRouter, managerRouter, restricted)
+server.use('/api/users', cryptoRouter, clientRouter, traderRouter, managerRouter, restricted)
 
 module.exports = server
