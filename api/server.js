40763d455d149cc8df14d90164ab5f62811342b5
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-27 19:43:21 -0500

    f

--- api/server.js
+++ api/server.js
@@ -1,43 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const helmet = require('helmet')
 
-// Routers
-const authRouter = require('../api/auth/auth-router');
-const clientRouter = require('../api/users/client-router');
-const traderRouter = require('../api/users/trader-router');
-const managerRouter = require('../api/users/manager-router');
-const cryptoRouter = require('../api/users/crypto-router');
+const { restricted } = require('../api/auth/auth-middleware')
 
-// Middleware
-const { restricted } = require('../api/auth/auth-middleware');
+const authRouter = require('../api/auth/auth-router')
+const clientRouter = require('../api/users/client-router')
+const traderRouter = require('../api/users/trader-router')
+const managerRouter = require('../api/users/manager-router')
+const cryptoRouter = require('../api/users/crypto-router')
 
-const server = express();
+const server = express()
 
-// Basic Middleware
-server.use(express.json());
-server.use(cookieParser());
-server.use(helmet());
 
-// CORS Configuration
-const corsOptions = {
-    origin: ['http://localhost:3000', 'https://yourproductiondomain.com'], // List all domains that are allowed to access or use '*'
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // if your site uses cookies/session
-};
 
-server.use(cors(corsOptions));
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+server.use(cookieParser())
 
-// Routes
-server.use('/api/auth', authRouter);  // Authentication routes
 
-// User-related routes with restricted middleware applied where necessary
-server.use('/api/users/crypto', cryptoRouter);  // Crypto-related routes
-server.use('/api/users/clients', restricted, clientRouter);  // Client routes, requires auth
-server.use('/api/users/traders', restricted, traderRouter);  // Trader routes, requires auth
-server.use('/api/users/managers', restricted, managerRouter);  // Manager routes, requires auth
+server.use('/api/auth', authRouter)
 
-// Export the configured server
-module.exports = server;
+server.use('/api/users', cryptoRouter, clientRouter, traderRouter, managerRouter)
+
+module.exports = server
