9685d659e80fe1369bdf64b8e858776a9c072c1e
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-19 10:31:19 -0600

    added cors options

--- api/server.js
+++ api/server.js
@@ -13,18 +13,39 @@ const managerRouter = require('../api/users/manager-router')
 
 const server = express()
 
+const allowedOrigins = [
+    'https://rev-bts.vercel.app', // production frontend
+    'http://localhost:3000',     // dev frontends you use
+
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Allow tools like Postman (no origin)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error('Not allowed by CORS'));
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false, // set to true ONLY if you use cookies / auth that require it
+};
 
 
 
+server.use(express.json());
+server.use(helmet());
+server.use(cors(corsOptions));
+server.options('*', cors(corsOptions));
+server.use(cookieParser());
 
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
-server.use(cookieParser())
 
+server.use('/api/auth', authRouter);
 
-server.use('/api/auth', authRouter)
-
-server.use('/api/users', clientRouter, traderRouter, managerRouter, restricted)
+server.use('/api/users', clientRouter, traderRouter, managerRouter, restricted);
 
 module.exports = server
